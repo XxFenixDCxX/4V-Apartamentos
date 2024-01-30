950f7a871f7c347e77e711f53f7224fbe7d560d2
Arkaitz Calvo Saldias <acalvosald@educacion.navarra.es>
2024-01-30 09:03:12 +0100

    Creados componentes de contacto y de apartamentos

---
+++ src/app/apartamentos/apartamentos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apartamentos',
+  standalone: true,
+  imports: [],
+  templateUrl: './apartamentos.component.html',
+  styleUrl: './apartamentos.component.scss'
+})
+export class ApartamentosComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+// app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ApartamentosComponent } from './apartamentos/apartamentos.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/apartamentos', pathMatch: 'full' },
+  { path: 'apartamentos', component: ApartamentosComponent },
+  { path: 'contacto', component: ContactoComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
