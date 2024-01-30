bf10e19bac33c70ef096151390fd5a38a69ca26e
Arkaitz Calvo Saldias <acalvosald@educacion.navarra.es>
2024-01-30 09:26:46 +0100

    Agregado footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MenuComponent],
+  imports: [CommonModule, RouterOutlet, MenuComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'arkaitz_calvo';
+  title = '4V Apartamentos';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,16 @@
-// app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ApartamentosComponent } from './apartamentos/apartamentos.component';
 import { ContactoComponent } from './contacto/contacto.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/apartamentos', pathMatch: 'full' },
   { path: 'apartamentos', component: ApartamentosComponent },
   { path: 'contacto', component: ContactoComponent },
+  { path: '', redirectTo: '/apartamentos', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
